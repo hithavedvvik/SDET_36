660afbcb460c2977ad0772142f7ce87627dd76c4
Lenovo ID3 <Lenovo ID3@Lenovo>
2022-07-19 12:08:39 +0530

    2nd commit

--- MAVENPRACTICE/src/test/java/practice/Practice1Test.java
+++ MAVENPRACTICE/src/test/java/practice/Practice1Test.java
@@ -7,15 +7,17 @@ public class Practice1Test {
 	
 	@Test
 	public void practice1Test() {
+		String PASSWORD= System.getProperty("password");
 		String URL = System.getProperty("url");
 		String BROWSER = System.getProperty("browser");
 		String USERNAM = System.getProperty("username");
-		String PASSWORD = System.getProperty("password");
+		String PASSWORD1 = System.getProperty("password");
 		System.out.println("Test1");
 		System.out.println("Test1"+URL);
 		System.out.println("Test1"+BROWSER);
 		System.out.println("Test1"+USERNAM);
 		System.out.println("Test1"+PASSWORD);
+		System.out.println("Test1"+PASSWORD1);
 		
 	}
 }
